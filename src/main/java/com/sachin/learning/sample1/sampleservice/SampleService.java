95e9349d984af79c44a844582dd112a2831c28c1
sachin-learning <sachin.learning.30@gmail.com>
2024-02-26 15:50:25 +0100

    Resolving issues

--- src/main/java/com/sachin/learning/sample1/sampleservice/SampleService.java
+++ src/main/java/com/sachin/learning/sample1/sampleservice/SampleService.java
@@ -1,16 +1,18 @@
 package com.sachin.learning.sample1.sampleservice;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import com.sachin.learning.util.SampleUtilities;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-@RestController
+@Controller
 public class SampleService {
 	
 	@RequestMapping(path="/getMessage", method=RequestMethod.GET)
